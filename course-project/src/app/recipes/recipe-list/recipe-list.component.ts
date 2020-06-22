8a2bad6389957a2035af4cb1963660bff4366bdc
haodang0000 <haodang0000@gmail.com>
2020-06-22 14:29:57 +0700

    video11 ngfor displaying list recipes

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[]=[
+    new Recipe('A test recipe','This is simply a test','https://img.taste.com.au/i6uNNUvx/taste/2018/02/mar-18_creamy-fettuccine-with-chicken-3000x2000-135690-1.jpg'),
+    new Recipe('A test recipe2','This is simply a test2','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
