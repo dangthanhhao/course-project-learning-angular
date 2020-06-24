56091f6cfcf684b72af4830dda9efc70be140ea8
Dang Thanh Hao <aszqsc@gmail.com>
2020-06-24 16:51:11 +0700

    Finshed chap3

--- course-project/src/app/shopping/shopping.component.ts
+++ course-project/src/app/shopping/shopping.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../model/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
-
+  ingredients:Ingredient[]=[
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatoes',10),
+    
+  ];
   constructor() { }
 
   ngOnInit(): void {
